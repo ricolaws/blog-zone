1c0a6384f97a9084f17fe7adbc0f77943d95dae7
Richard Laws <richard.laws@eyecuelab.com>
2022-10-06 20:36:28 -0700

    add users create action and link_to signup from home.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController < ApplicationController
     end
 
     def create
-
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Hi #{@user.username}, Welcome to Blog Zone!"
+            redirect_to articles_path
+        else
+            render "new", status: :unprocessable_entity
+        end
     end
 
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end 
